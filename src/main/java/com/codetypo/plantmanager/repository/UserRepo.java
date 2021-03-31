27fc7ad78602ce9b3601a6ba18b8ff58102085f0
Mikolaj <mikus1001@gmail.com>
2021-04-01 00:13:14 +0200

    findUserByEmail in UserRepo

--- src/main/java/com/codetypo/plantmanager/repository/UserRepo.java
+++ src/main/java/com/codetypo/plantmanager/repository/UserRepo.java
@@ -4,4 +4,5 @@ import com.codetypo.plantmanager.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository <User,Long> {
+    User findUserByEmail(String email);
 }
